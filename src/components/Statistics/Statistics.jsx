d993612fbe9af5d6bdb654cdd0fc924f60548cc2
belyy10 <maximgyda10@icloud.com>
2022-10-28 21:18:42 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 import { getRandomHexColor } from '../utils/randomColor';
 
-export const Statistics = ({ stats, title}) => {
+export const Statistics = ({ stats, title }) => {
   return (
     <section className={css.statistics}>
       {title && <h2 className={css.title}>{title}</h2>}
@@ -30,4 +30,6 @@ Statistics.propTypes = {
       percentage: PropTypes.number.isRequired,
     }).isRequired
   ),
+  isRequired,
+  title: PropTypes.string.isRequired,
 };
